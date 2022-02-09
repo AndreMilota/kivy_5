21db71f6de567853dd8d499112d911bc96ac5115
Andre Milota <milota@mindgrip.com>
2022-02-09 10:33:25 -0800

    got hello world working

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+print("User Current Version:-", sys.version)
+
+class MyApp(App):
+    def build(self):
+        return Label(text="Hello World")
+
+if __name__ == '__main__':
+    MyApp().run()
+
