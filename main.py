de2b6e6e90a23cd0152c4648fa5ac9336bc2488d
Andre Milota <milota@mindgrip.com>
2022-02-09 11:08:40 -0800

    got drag to work

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ from kivy.uix.label import Label
 print("User Current Version:-", sys.version)
 
 class Touch(Widget):
+    def __init__(self, **kwargs):
+        super(Touch, self).__init__(**kwargs)
+
+        with self.canvas:
+            Color(1, 0, 0, 1, mode='rgba')
+            self.rect = Rectangle(pos=(0, 0), size=(50, 50))
 
     def on_touch_down(self, touch):
         print("Mouse down", touch)
 
     def on_touch_move(self, touch):
+        self.rect.pos = touch.pos
         print("Mouse move", touch)
 
     def on_touch_up(self, touch):
